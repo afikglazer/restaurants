e91efdeb8d741eacecfef459130665f6ccf16207
Afik Glazer <afik.glazer@kryonsystems.com>
2024-12-30 18:49:55 +0200

    WIP - MongoDB

--- terraform.tf
+++ terraform.tf
@@ -4,28 +4,44 @@ provider "azurerm" {
   subscription_id = "2a43cdb3-2031-4411-8951-551e7e525852"
 }
 
-# Create the resource group
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
   location = "East US"
 }
 
-# Create the storage account
-resource "azurerm_storage_account" "example" {
-  name                     = "examplestorageacct"
-  resource_group_name       = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
-  account_tier              = "Standard"
-  account_replication_type = "LRS"
+resource "azurerm_cosmosdb_account" "example" {
+  name                = "example-cosmosdb"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  offer_type          = "Standard"
+  kind                = "MongoDB"
+
+  consistency_policy {
+    consistency_level = "Session"
+  }
+
+  geo_location {
+    location          = azurerm_resource_group.example.location
+    failover_priority = 0
+  }
+
+  capabilities {
+    name = "EnableMongo"
+  }
 }
 
-# Create the table storage
-resource "azurerm_storage_table" "example" {
-  name                 = "exampletable"
-  storage_account_name = azurerm_storage_account.example.name
+resource "azurerm_cosmosdb_mongo_database" "example" {
+  name                = "exampledb"
+  resource_group_name = azurerm_resource_group.example.name
+  account_name        = azurerm_cosmosdb_account.example.name
+  throughput          = 400
 }
 
-# Output the connection string
-output "storage_account_connection_string" {
-  value = azurerm_storage_account.example.primary_connection_string
+resource "azurerm_cosmosdb_mongo_collection" "example" {
+  name                = "example-collection"
+  resource_group_name = azurerm_resource_group.example.name
+  account_name        = azurerm_cosmosdb_account.example.name
+  database_name       = azurerm_cosmosdb_mongo_database.example.name
+  shard_key           = "_id"
+  throughput          = 400
 }
