175f3a87c3bce57196f9c745202fcefce55f572a
Afik Glazer <afik.glazer@kryonsystems.com>
2024-12-31 20:42:25 +0200

    WIP - Docker fixed

--- container-terraform/main.py
+++ container-terraform/main.py
@@ -72,4 +72,4 @@ def recommend_restaurant():
     return jsonify({"message": "No restaurant matches the criteria."}), 404
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
